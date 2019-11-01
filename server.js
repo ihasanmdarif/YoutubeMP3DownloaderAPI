d45b874e8c3c09c0a7d78bd047e56db9bf123b7e
iarifs <arif.cvc13@gmail.com>
2019-10-31 23:44:27 -0500

    fix getting empty array

--- server.js
+++ server.js
@@ -5,15 +5,12 @@ var app = express();
 const cheerio = require("cheerio");
 const ytdl = require("ytdl-core");
 
-var VideoInfoArray = [];
-
-var AudioInfoArray = [];
-
 //start the server in 80 port
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, err => {
-    if(err) throw err;
-    console.log("%c Server running", "color: green");
+    if (err) throw err;
+    console.log("Server running", "color: green");
 });
 
 // Calling homepage ang get this page
@@ -34,9 +31,7 @@ app.get("/search", (req, res) => {
             message: "search with any video name"
         });
     } else {
-        getSearchedlist(query);
-        res.json(VideoInfoArray);
-        VideoInfoArray = [];
+        getSearchedlist(query, res);
     }
 });
 
@@ -44,25 +39,23 @@ app.get("/search", (req, res) => {
 app.get("/download", (req, res) => {
 
     var query = req.query.id;
-
     if (query == undefined) {
         res.status(404).send({
             success: "false",
             message: "search with any video link"
         });
     } else {
-        getAudioLink(query);
-        res.json(AudioInfoArray);
-        AudioInfoArray = [];
+        getAudioLink(query,res);
     }
 
 })
 
 
-function getSearchedlist(query) {
-
+function getSearchedlist(query, res) {
     request(`https://www.youtube.com/results?search_query=${query}&pbj=1`,
         function (error, response, body) {
+            var arrayOfVideos = [];
+            var video;
             const $ = cheerio.load(body);
             for (var i = 0; i <= 10; i++) {
                 var videoid = $(`#results ol li:nth-child(2) li:nth-child(${i}) div:nth-child(1)`).find("a").attr("href");
@@ -71,42 +64,48 @@ function getSearchedlist(query) {
                     !videoid.includes("/results?search_query") &&
                     !videoid.includes("googleadservices.com")) {
 
-                    var imglink = $(`#results ol li:nth-child(2) li:nth-child(${i}) div:nth-child(1)`).find("img").attr("src");
                     var title = $(`#results ol li:nth-child(2) li:nth-child(${i}) div:nth-child(2)`).find("a").attr("title");
-                    var video = new VideoInfo(title, videoid, imglink);
+                    var imglink = $(`#results ol li:nth-child(2) li:nth-child(${i}) div:nth-child(1)`).find("img").attr("data-thumb");
 
-                    if (VideoInfoArray.length < 10) {
-                        VideoInfoArray.push(video);
+                    if (imglink === undefined) {
+                        var newImageLink = $(`#results ol li:nth-child(2) li:nth-child(${i}) div:nth-child(1)`).find("img").attr("src");
+                        video = new VideoInfo(title, videoid, newImageLink);
+                    } else {
+                        video = new VideoInfo(title, videoid, imglink);
                     }
+                    arrayOfVideos.push(video);
                 }
             }
+            res.json(arrayOfVideos);
         });
 }
 
 
-function getAudioLink(query) {
-
+function getAudioLink(query,res) {
     ytdl.getInfo(query, (err, info) => {
+        var AudioInfoArray = [];
         info.formats.forEach(element => {
             if (element.itag === "140" ||
                 element.itag === "171" ||
                 element.itag === "251") {
-                    var audio = new AudioInfo(element.itag,element.url);
-                    if(AudioInfoArray.length < 3){
-                        AudioInfoArray.push(audio);
-                    }
+                var audio = new AudioInfo(element.itag, element.url, info.player_response.videoDetails.title);
+                if (AudioInfoArray.length < 4 ) {
+                    AudioInfoArray.push(audio);
+                }
             }
         })
+        res.json(AudioInfoArray)
     });
 }
 
 function VideoInfo(title, videoId, imgLink) {
-        this.title = title,
+    this.title = title,
         this.id = videoId,
         this.img = imgLink
 }
 
-function AudioInfo(itag, link) {
+function AudioInfo(itag, link, title) {
     this.itag = itag;
     this.link = link;
+    this.title = title;
 }
